3181b68f4b94fdcf0da8103d8c89917e4b49b6a0
Diana Pavel <dianapav@gmail.com>
2022-02-22 15:34:27 +0000

    added the route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   devise_for :users
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+
   resources :companies
 
   resources :goals do
