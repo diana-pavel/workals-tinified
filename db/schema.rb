cb285f5eb4621cf181fee10055d704c8cd34cb52
Diana Pavel <dianapav@gmail.com>
2022-02-23 09:20:41 +0000

    added confirmable to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_142040) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_23_075309) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
@@ -43,6 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_142040) do
     t.boolean "admin"
     t.string "first_name"
     t.string "last_name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
