1766528cf648ecb43647c9ab452407e5751152f9
Diana Pavel <dianapav@gmail.com>
2022-02-23 17:53:43 +0000

    edited the goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,5 +4,64 @@ class GoalsController < ApplicationController
     @goals = Goal.all
   end
 
+  def show
+  end
 
+  # GET /companies/new
+  def new
+    @goal = Goal.new
+  end
+
+  # GET /companies/1/edit
+  def edit
+  end
+
+  # POST /companies or /companies.json
+  def create
+    @goal = Goal.new(goal_params)
+
+    respond_to do |format|
+      if @goal.save
+        format.html { redirect_to goal_url(@goal), notice: "Goal was successfully created." }
+        format.json { render :show, status: :created, location: @goal }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @goal.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /companies/1 or /companies/1.json
+  def update
+    respond_to do |format|
+      if @goal.update(goal_params)
+        format.html { redirect_to goal_url(@goal), notice: "Goal was successfully updated." }
+        format.json { render :show, status: :ok, location: @goal }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @goal.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /companies/1 or /companies/1.json
+  def destroy
+    @goal.destroy
+
+    respond_to do |format|
+      format.html { redirect_to goal_url, notice: "Goal was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_goal
+      @goal = Goal.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def goal_params
+      params.require(:goal).permit(:goal, :description, :user_id)
+    end
 end
