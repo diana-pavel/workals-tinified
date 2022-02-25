797cd3e8c9f2cfebcb630562693b813cf9520740
Diana Pavel <dianapav@gmail.com>
2022-02-25 09:29:57 +0000

    added invitable to devise

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "bootsnap", require: false
 
 gem "devise", "~> 4.8"
 gem "pundit"
+gem 'devise_invitable', '~> 2.0.0'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :invitable, invite_for: 4.weeks
 
 has_many :goals, dependent: :destroy
 has_many :companies
